e5e56a3c5bc5d6866d909e833333d6931d890d8d
teomac <liquidinobusiness@gmail.com>
2025-09-05 21:37:08 +0200

    Game complete

---
+++ js/javascript.js
@@ -0,0 +1,96 @@
+console.log("Test");
+
+var humanScore = 0;
+var computerScore = 0;
+
+function playGame () {
+
+  const validChoices = ["Rock", "Paper", "Scissors"];
+
+  function getChoiceValue(choice) {
+    let value;
+    if (choice === "Rock") {
+      value = 1;
+    }
+    else if (choice === "Paper") {
+      value = 2;
+    }
+    else if (choice === "Scissors") {
+      value = 3;
+    }
+
+    else {
+      value = null;
+    }
+
+    return value;
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanSelection;
+    computerChoice = computerSelection;
+    let humanChoiceValue = getChoiceValue(humanChoice);
+    let computerChoiceValue = getChoiceValue(computerChoice);
+
+    if (humanChoiceValue !== null && computerChoiceValue !== null) {
+
+      if (computerChoiceValue > humanChoiceValue && (computerChoiceValue+humanChoiceValue)%2 !== 0) {
+        console.log("Computer wins!");
+        computerScore++;
+      }
+  
+      else if (computerChoiceValue === humanChoiceValue) {
+        console.log("It's a tie!");
+      }
+  
+      else {
+        console.log("You won!");
+        humanScore++;
+      }
+    }
+
+    console.log("Human score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+  }
+  
+  function getHumanChoice () {
+    let choice = prompt("Enter your Choice");
+    if (validChoices.includes(choice)) {
+      console.log("Player Choice: " + choice);
+      return choice;
+    }
+
+    else if (choice === null) {
+      console.log("Game Cancelled");
+      return choice;
+    }
+
+    else {
+      console.log("Invalid Choice");
+      return getHumanChoice();
+    }
+  }
+  
+  function getComputerChoice() {
+    let choiceNumber = Math.random();
+    let choice;
+    if (choiceNumber < 1/3) {
+      choice = "Rock";
+    }
+    else if (choiceNumber < 2/3) {
+      choice = "Paper";
+    }
+
+    else {
+      choice = "Scissors";
+    }
+    console.log("Computer Choice: " + choice);
+    return choice;
+  }
+
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+
+}
